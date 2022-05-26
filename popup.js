28dc726e5fa1977edf8d80e00ea8c909d859775f
Safin-Ahmed <safin.ahmed2000@gmail.com>
2022-05-26 16:14:14 +0600

    Improved

--- popup.js
+++ popup.js
@@ -1,4 +1,13 @@
-document.getElementById("name").addEventListener("keyup", () => {
-  document.getElementById("greet").innerText =
-    "Hello World " + document.getElementById("name").value;
+const inputArea = document.getElementById("notearea");
+const events = ["change", "paste"];
+
+chrome.storage.sync.get("note", (note) => {
+  inputArea.value = note.note;
 });
+
+events.forEach((ev) =>
+  inputArea.addEventListener(ev, () => {
+    let noteText = inputArea.value;
+    chrome.storage.sync.set({ note: noteText });
+  })
+);
